12f05f61eaea7af2ba600bdde1960bbf5adc26f0
orco6 <orcohen733@gmail.com>
2021-11-29 15:55:07 +0200

    init

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -26,7 +26,7 @@ app.use(cors());
 
 
 
-app.get('/', (req,res) =>{res.send('it is workin!')})
+app.get('/', (req,res) =>{res.send('it is working!')})
 app.post('/signin',signin.handleSignin(db,bcrypt));
 app.post('/register',(req,res) =>{register.handleRegister(req,res,db,bcrypt) })
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req,res,db)});
